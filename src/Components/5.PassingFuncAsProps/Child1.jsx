b2ae7238cf239d47328a951034961470b3f1f8b5
nadeem35 <nadeem.cse72@gmail.com>
2026-03-07 23:45:08 +0530

    update passing props method

--- src/Components/5.PassingFuncAsProps/Child1.jsx
+++ src/Components/5.PassingFuncAsProps/Child1.jsx
@@ -12,33 +12,34 @@
 
 // ==================   🔹 2.  =================
 
-// Child.js
+// // Child.js
 
-import React from "react";
+// import React from "react";
 
-const Child1 = ({ greeting }) => {
-  return (
-    <div>
-      <button onClick={() => greeting("I am data from Child!")}>
-        Send Data to Parent
-      </button>
-    </div>
-  );
-};
+// const Child1 = ({ greeting }) => {
+//   return (
+//     <div>
+//       <button onClick={() => greeting("I am data from Child!")}>
+//         Send Data to Parent
+//       </button>
+//     </div>
+//   );
+// };
 
-export default Child1;
+// export default Child1;
 
 // =================  🔹 3.   =================
 
 
-// // Child.js
-// function Child1({ onUpdate }) {
-//   return (
-//     <>
-//       <button onClick={() => onUpdate("Nadeem")}>Send Name</button>
-//       <button onClick={() => onUpdate("Uzair")}>Send Another Name</button>
-//     </>
-//   );
-// }
+// Child.js
+function Child1({ onUpdate }) {
+  return (
+    <>
+      <button onClick={() => onUpdate("Nadeem")}>Send Name</button>
+      <button onClick={() => onUpdate("Uzair")}>Send Another Name</button>
+    </>
+  );
+}
 
-// export default Child1;
+export default Child1;
+
--- src/Components/5.PassingFuncAsProps/Parent1.jsx
+++ src/Components/5.PassingFuncAsProps/Parent1.jsx
@@ -22,43 +22,45 @@
 // Sometimes, you want the child to pass data back to the parent.
 
 // // Parent.js
-import React from "react";
-import Child1 from "./Child1";
+// import React from "react";
+// import Child1 from "./Child1";
 
-const Parent1 = () => {
+// const Parent1 = () => {
 
-  const showAlert = (msg) => {
-    alert(" Parent Received : " + msg);
-  };
+//   const showAlert = (msg) => {
+//     alert(" Parent Received : " + msg);
+//   };
 
-  return (
-    <div>
-      <Child1 greeting={showAlert}></Child1>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h2>Parent Component</h2>
+//       <Child1 greeting={showAlert}></Child1>
+//     </div>
+//   );
+// };
 
-export default Parent1;
+// export default Parent1;
 
 
 // =================  🔹 3.   =================
 
-// // 🔹 3. Using State + Function Props
-// // Parent wants to update its state based on child actions
+// 🔹 3. Using State + Function Props
+// Parent wants to update its state based on child actions
 
-// // Parent.js
-// import { useState } from "react";
-// import Child1 from "./Child1";
+// Parent.js
+import { useState } from "react";
+import Child1 from "./Child1";
 
-// function Parent1() {
-//   const [name, setName] = useState("");
+function Parent1() {
+  const [name, setName] = useState("Saifi");
 
-//   return (
-//     <div>
-//       <h2>Selected Name: {name}</h2>
-//       <Child1 onUpdate={setName} />
-//     </div>
-//   );
-// }
+  return (
+    <div>
+      <h3>Selected Name: {name}</h3>
+      <Child1 onUpdate={setName} />
+    </div>
+  );
+}
 
-// export default Parent1;
+export default Parent1;
+
