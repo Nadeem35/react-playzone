5edfe037e181ddbb20234091dd13858568ea5596
nadeem35 <nadeem.cse72@gmail.com>
2026-03-08 23:59:29 +0530

    add pages

--- src/Components/15.JWT/Navbar.jsx
+++ src/Components/15.JWT/Navbar.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const Navbar = () => {
   return (
     <div>
       <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Service</li>
-        <li>Contact</li>
+        <li>
+          <Link to="home">
+            Home
+          </Link>
+        </li >
+        <li>
+          <Link to="About">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="Service">
+            Service
+          </Link>
+        </li>
+        <li to="Contact" Link="/Contact">Contact</li>
       </ul>
-    </div>
+    </div >
   )
 }
 
