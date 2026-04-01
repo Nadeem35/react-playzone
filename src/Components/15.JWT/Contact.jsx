8640341710f92f05aea94af50796ba6758fba061
nadeem35 <nadeem.cse72@gmail.com>
2026-04-01 23:45:42 +0530

    update code

--- src/Components/15.JWT/Contact.jsx
+++ src/Components/15.JWT/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <>
-      <h2 className='text-center'>This is contact page</h2>
+      <h2 className='text-center text-success font-bold'>This is contact page</h2>
     </>
   )
 }
