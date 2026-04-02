394014dca0c7e96ea76e3ebe3c78ebb0ebed7226
nadeem35 <nadeem.cse72@gmail.com>
2026-04-02 23:55:35 +0530

    code update

--- src/Components/15.JWT/About.jsx
+++ src/Components/15.JWT/About.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      <h2 className='text-bold text-center'>About Page</h2>
-      <img className='d-flex mx-auto w-60' src="https://promexmarkets.com/images/about.png" />
+      <h2 className='text-bold text-center'>About Us Page</h2>
+      <img className='d-flex mx-auto w-60' src="https://img.freepik.com/premium-vector/about-us_773186-1364.jpg?semt=ais_hybrid&w=740&q=80" />
+      {/* <img className='d-flex mx-auto w-60' src="https://promexmarkets.com/images/about.png" /> */}
     </div>
   )
 }
