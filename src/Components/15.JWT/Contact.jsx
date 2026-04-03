25191ea5de20ee42c220df305a77797e9acfdd71
nadeem35 <nadeem.cse72@gmail.com>
2026-04-03 23:39:56 +0530

    update layout

--- src/Components/15.JWT/Contact.jsx
+++ src/Components/15.JWT/Contact.jsx
@@ -4,6 +4,9 @@ const Contact = () => {
   return (
     <>
       <h2 className='text-center text-success font-bold'>This is contact page</h2>
+      <img
+        className='d-flex mx-auto w-50'
+        src='https://in.thedollarbusiness.com/assets/articles/2016/11/48424_India,_Palestine_372.jpg' />
     </>
   )
 }
--- src/Components/15.JWT/Home.jsx
+++ src/Components/15.JWT/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <div>
       <h2 className='text-center text-bold'>Home Page</h2>
       <img
-        className='d-flex mx-auto w-80'
+        className='d-flex mx-auto w-50'
         src='https://cdnuploads.aa.com.tr/uploads/Contents/2018/03/17/thumbs_b_c_65405eccdd9c7c57aafd2a674ede221c.jpg?v=155552' />
     </div>
   )
