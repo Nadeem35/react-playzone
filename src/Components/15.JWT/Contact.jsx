60229f2f00c080e3da225525fe8ce5ac73957059
nadeem35 <nadeem.cse72@gmail.com>
2026-04-06 23:55:53 +0530

    changes

--- src/Components/15.JWT/Contact.jsx
+++ src/Components/15.JWT/Contact.jsx
@@ -8,7 +8,8 @@ const Contact = () => {
         className='d-flex mx-auto w-50'
         src='https://in.thedollarbusiness.com/assets/articles/2016/11/48424_India,_Palestine_372.jpg' />
       <br />
-      <iframe className='d-flex mx-auto' width="560" height="315" src="https://www.youtube.com/embed/Lptt_72wXUg?si=CiI5dBokU-yGpPPG" title="Nadeem" frameborder="0" allowfullscreen></iframe>
+
+      <iframe className='d-flex mx-auto' width="560" height="315" src="https://www.youtube.com/embed/Lptt_72wXUg?si=CiI5dBokU-yGpPPG" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </>
   )
 }
