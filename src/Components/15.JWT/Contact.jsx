3fda0b74177a7575f7b57ff661bbecf6c74d0bba
nadeem35 <nadeem.cse72@gmail.com>
2026-04-06 23:52:05 +0530

    save code

--- src/Components/15.JWT/Contact.jsx
+++ src/Components/15.JWT/Contact.jsx
@@ -7,6 +7,8 @@ const Contact = () => {
       <img
         className='d-flex mx-auto w-50'
         src='https://in.thedollarbusiness.com/assets/articles/2016/11/48424_India,_Palestine_372.jpg' />
+      <br />
+      <iframe className='d-flex mx-auto' width="560" height="315" src="https://www.youtube.com/embed/Lptt_72wXUg?si=CiI5dBokU-yGpPPG" title="Nadeem" frameborder="0" allowfullscreen></iframe>
     </>
   )
 }
