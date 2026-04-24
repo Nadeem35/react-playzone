ba64aa938e2833dca52b03bdeecf2efe02cee70e
nadeem35 <nadeem.cse72@gmail.com>
2026-04-24 23:49:09 +0530

    update

--- src/Components/9.useRefUses/Useref.jsx
+++ src/Components/9.useRefUses/Useref.jsx
@@ -4,26 +4,26 @@ React me directly document.getElementById() use karna best practice nahi hai.
 Iske liye hum useRef use karte hai.
 */
 
-import React, { useRef } from "react";
+// import React, { useRef } from "react";
 
-const Useref = () => {
-  const inputRef = useRef();
+// const Useref = () => {
+//   const inputRef = useRef();
 
-  const inputFocus = () => {
-    inputRef.current.focus();
-  };
+//   const inputFocus = () => {
+//     inputRef.current.focus();
+//   };
 
-  console.log(inputRef);
+//   console.log(inputRef);
 
-  return (
-    <>
-      <input type="text" ref={inputRef} placeholder="Enter name" />
-      <button onClick={inputFocus}>Submit</button>
-    </>
-  );
-};
+//   return (
+//     <>
+//       <input type="text" ref={inputRef} placeholder="Enter name" />
+//       <button onClick={inputFocus}>Submit</button>
+//     </>
+//   );
+// };
 
-export default Useref;
+// export default Useref;
 
 // ---------------------------------------
 
@@ -31,25 +31,25 @@ export default Useref;
 2. Storing Values Without Re-render
 useState me value change hone par component re-render hota hai.
       Agar hume value store karni hai bina re-render ke → useRef use karte hai.
-      */
+*/
 
-// import { useRef, useState } from "react";
+import { useRef, useState } from "react";
 
-// function Useref() {
-//   const [count, setCount] = useState(0);
-//   const renderCount = useRef(0);
-//   renderCount.current = renderCount.current + 1;
+function Useref() {
+  const [count, setCount] = useState(0);
+  const renderCount = useRef(0);
+  renderCount.current = renderCount.current + 1;
 
-//   return (
-//     <>
-//       <h2>Clicked : {count}</h2>
-//       <h2>Component Rendered: {renderCount.current}</h2>
-//       <button onClick={() => setCount(count + 1)}>Count</button>
-//     </>
-//   );
-// }
+  return (
+    <>
+      <h2>Clicked : {count}</h2>
+      <h2>Component Rendered: {renderCount.current}</h2>
+      <button onClick={() => setCount(count + 1)}>Count</button>
+    </>
+  );
+}
 
-// export default Useref;
+export default Useref;
 
 // ----------------------------
 
